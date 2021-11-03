4f8521b143a30d0b21e09557bb829f90edf98fc6
sam <sammtcbn@gmail.com>
2021-11-04 00:28:47 +0800

    fix wording

--- windows_service_create.bat
+++ windows_service_create.bat
@@ -3,7 +3,7 @@ set currdir=%~dp0
 IF "%currdir:~-1%"=="\" SET currdir=%currdir:~0,-1%
 cd /d "%currdir%"
 
-C:\nginx\nssm install nginx "nginx"
+C:\nginx\nssm install nginx "nginx.exe"
 C:\nginx\nssm set nginx AppDirectory "C:\nginx"
 rem C:\nginx\nssm set nginx AppParameters "-c conf/nginx.conf -p ."
 C:\nginx\nssm start nginx
